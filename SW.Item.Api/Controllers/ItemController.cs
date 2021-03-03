92d2fee83eac2b41a202ba2cf1c65587e6c2f3cf
Mehdi Marouani <Mehdi.Marouani@vistaprint.com>
2021-03-03 22:00:45 +0100

    - init project

---
+++ SW.Item.Api/Controllers/ItemController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SW.Item.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemController : ControllerBase
+    {
+        private readonly ILogger<ItemController> _logger;
+
+        public ItemController(ILogger<ItemController> logger)
+        {
+            _logger = logger;
+        }
+
+    }
+}
